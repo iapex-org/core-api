89ac7447d2784b18d302bffb173b3a5b618f4352
ChristianSp <christiansp7231@gmail.com>
2024-12-02 10:28:52 -0600

    Update CORS configuration to allow additional origin for improved API access

--- src/main/java/com/iapex/IapexApiRestApplication.java
+++ src/main/java/com/iapex/IapexApiRestApplication.java
@@ -21,9 +21,9 @@ public class IapexApiRestApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-				        .allowedOrigins("http://localhost:4200")
-				        .allowedMethods("*")
-				        .allowedHeaders("*");
+						.allowedOrigins("http://localhost:4200", "http://localhost:8100")
+						.allowedMethods("*")
+						.allowedHeaders("*");
 			}
 		};
 	}
